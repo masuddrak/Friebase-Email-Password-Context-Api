b049623a1eb9cacc1d7ccf292fc3c826a09c2994
masud <masudranaacb@gmail.com>
2024-04-01 00:08:24 +0600

    create sign in and sign up form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,44 @@
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
+    const handelSignIn=(e)=>{
+        e.preventDefault()
+        const email=e.target.email.value
+        const password=e.target.password.value
+        console.log(email,password)
+    }
     return (
         <div>
-            this is SignIn
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col ">
+                    <div className="text-center mb-3">
+                        <h1 className="text-3xl font-bold">Sing In now!</h1>
+                    </div>
+                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handelSignIn} className="card-body">
+                        
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Sign Up</button>
+                            </div>
+                        </form>
+                        <div className="mb-3">
+                            <p className="text-center">Are Your New? <Link to="/signUp" className="underline text-green-600">Sign Up</Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
